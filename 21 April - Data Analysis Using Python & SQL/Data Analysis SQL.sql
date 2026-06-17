8fc635b490a0ec85e830ec6aabcea2774f716b54
Pratik Lagishetty <147292950+Blvackk@users.noreply.github.com>
2026-06-17 20:11:41 +0530

    Add files via upload

---
+++ 21 April - Data Analysis Using Python & SQL/Data Analysis SQL.sql
@@ -0,0 +1,8 @@
+select * FROM  dataset_1 ;
+
+SELECT weather , temperature from dataset_1 ;
+SELECT * from dataset_1 limit 10;
+select distinct passanger from dataset_1 ;
+
+select * from dataset_1 where destination = 'Home';
+select * from dataset_1 order by coupon ;
